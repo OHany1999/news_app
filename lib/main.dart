3de1c41526fbff8f21e97c472bdb26a095bcfaee
OHany1999 <omar1999410@gmail.com>
2022-12-19 23:08:20 +0200

    initial_app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/layout/home_layout.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeLayout.routeName,
+      routes: {
+        HomeLayout.routeName:(context)=>HomeLayout(),
+      },
+
+    );
+  }
+}
